808ba2098f90553319924ba9d56ddb0efdc89b5d
PeakyBlinder <93531354+DonatoReis@users.noreply.github.com>
2022-04-18 21:43:48 -0300

    Update ghostrecon.sh

--- ghostrecon.sh
+++ ghostrecon.sh
@@ -128,8 +128,8 @@ dg_menu() {
 report_tools() {
   tools[mrx]='Mrx Scan Subdomains|subfinder findomain-linux assetfinder|for log in "$logdir/"{assetfinder,findomain,subfinder}.log; do > "$log"; done; sleep 2;findomain-linux -q -t "$domain" > "$logdir/findomain.log"; sleep 2; subfinder -d "$domain" -silent -t 40 -o "$logdir/subfinder.log"; sleep 2; assetfinder -subs-only "$domain" > "$logdir/assetfinder.log"; sort -u "$logdir/"{assetfinder,findomain,subfinder}.log -o "$logfile"; httpx -silent < "$logfile" > "$logdir/${dtreport}httpx.log"'
   tools[dirsearch]='Dirsearch|dirsearch|xargs -L1 python3 /usr/local/dirsearch/dirsearch.py -q -e php,aspx,jsp,html,zip,jar -x 404-499,500-599 -w "$dicc" --random-agent -o "$logfile" -u < <(httpx -silent <<< "$domain"); sleep 2'
-  tools[feroxbuster]='Feroxbuster Scan sub-directories|feroxbuster|feroxbuster -q -x php,asp,aspx,jsp,html,zip,jar -t 80 -n -w "$dicc" -o "$logfile" -u "$domain"'
-  tools[whatweb]='Whatweb|whatweb|whatweb -q -t 60 --no-errors "$domain" --log-brief="$logfile"'
+  tools[feroxbuster]='Feroxbuster Scan sub-directories|feroxbuster|feroxbuster -q -x php,asp,aspx,jsp,html,zip,jar --auto-tune -n --random-agent -w "$dicc" -o "$logfile" -u "$domain"'
+  tools[whatweb]='Whatweb|whatweb|whatweb -q -t 50 --no-errors "$domain" --log-brief="$logfile"'
   tools[owasp]='Owasp Getallurls|waybackurls uro anew|cat "$logdir/${dtreport}httpx.log" | waybackurls | uro | anew | sort -u > "$logfile"'
   tools[crt]='Certificate Search|curl|curl -s "https://crt.sh/?q=%25.${domain}&output=json" | anew > "$logfile"'
   tools[nmap]='Nmap Ports|nmap|nmap -sS -sCV "$domain" -T4 -Pn -oN "$logfile"'

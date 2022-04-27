eefd35ad309a95f64adec83cfe85a8ca65728628
NRZ Code <nrzcode@protonmail.com>
2022-04-27 14:00:30 -0300

    Rename Kraken

--- install.sh
+++ install.sh
@@ -135,12 +135,12 @@ init_install() {
   printf "\n${CBold}${CFGWhite}=====================================================>${CReset}\n\n"
   if [[ ! -f $HOME/.local/.arno_init_install_successful ]]; then
     packages='python3-pip apt-transport-https curl libcurl4-openssl-dev libssl-dev jq ruby-full libcurl4-openssl-dev ruby libxml2 libxml2-dev libxslt1-dev ruby-dev dkms build-essential libgmp-dev hcxdumptool zlib1g-dev perl zsh fonts-powerline libio-socket-ssl-perl libdbd-sqlite3-perl libclass-dbi-perl libio-all-lwp-perl libparallel-forkmanager-perl libredis-perl libalgorithm-combinatorics-perl gem git cvs subversion bzr mercurial libssl-dev libffi-dev python-dev-is-python3 ruby-ffi-yajl python-setuptools libldns-dev rename docker.io parsero apache2 ssh tor privoxy proxychains4 aptitude synaptic lolcat dialog golang-go graphviz virtualenv reaver bats openssl cargo cmake'
+    wget -O /tmp/go1.18.1.linux-amd64.tar.gz https://go.dev/dl/go1.18.1.linux-amd64.tar.gz
+    rm -rf /usr/local/go
+    tar -C /usr/local -xzf /tmp/go1.18.1.linux-amd64.tar.gz
+    ln -sf /usr/local/go/bin/go /usr/local/bin/go
     case $distro in
       Ubuntu)
-        wget -O /tmp/go1.18.1.linux-amd64.tar.gz https://go.dev/dl/go1.18.1.linux-amd64.tar.gz
-        rm -rf /usr/local/go
-        tar -C /usr/local -xzf /tmp/go1.18.1.linux-amd64.tar.gz
-        ln -s /usr/local/go/bin/go /usr/local/bin/go
         packages+=' chromium-browser whois'
         ;;
       Kali)
@@ -149,8 +149,7 @@ init_install() {
         ;;
     esac
     apt -y install $packages
-    pip3 install --upgrade pip osrframework
-    pip3 install py-altdns==1.0.2 requests wfuzz holehe twint droopescan uro arjun dnsgen s3scanner emailfinder pipx one-lin3r win_unicode_console aiodnsbrute webscreenshot dnspython netaddr git-dumper --retries 10
+    pip3 install --upgrade pip osrframework py-altdns==1.0.2 requests wfuzz holehe twint droopescan uro arjun dnsgen s3scanner emailfinder pipx one-lin3r win_unicode_console aiodnsbrute webscreenshot dnspython netaddr git-dumper
     gem install typhoeus opt_parse_validator blunder wpscan
     mkdir -p "$HOME/.local"
     : > $HOME/.local/.arno_init_install_successful

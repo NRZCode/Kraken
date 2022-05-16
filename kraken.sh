4045cc73135d907457c838f4695456a5b7c70d97
PeakyBlinder <93531354+DonatoReis@users.noreply.github.com>
2022-05-16 09:33:18 -0300

    Update kraken.sh

--- kraken.sh
+++ kraken.sh
@@ -245,7 +245,6 @@ report_tools() {
   tools[nmap]='ports|nmap|nmap -sS -sCV "$target_domain" -T4 -Pn -oN "$logfile"'
   tools[nmap-cvss]='vulnerability|nmap|nmap -sV --script vulners --script-args mincvss=1.0 "$target_domain" -oN "$logfile"'
   tools[fnmap]='ports|nmap|nmap -n -Pn -sS "$target_domain" -T4 --open -sV -oN "$logfile"'
-  tools[email-search]='email-search|EmailHarvester emailfinder holehe|EmailHarvester -d $target_domain -e all -s $logdir/${dtreport}emailharvester.log; emailfinder -d $target_domain > $logdir/${dtreport}emailfinder.log; while read mail; do holehe --no-clear --only-used --no-color "$mail"; done < <(cat $logdir/${dtreport}email{harvester,finder}.log) > "$logfile"'
 }
 
 report() {
@@ -559,7 +558,7 @@ run() {
     banner
 
     # Tools for report
-    run_tools nmap nmap-cvss email-search
+    run_tools nmap nmap-cvss
     [[ $anon_mode == 1 ]] && anonsurf start &> /dev/null
     run_tools mrx whatweb owasp crt ${selection,,}
 
